b4e765e59952b91e9b364d349dbe370a15833762
yguillenparedes <yguillenparedes@gmail.com>
2021-08-05 19:52:39 +0000

    Mi tercer intento

--- src/models.py
+++ src/models.py
@@ -8,26 +8,48 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    userCode = Column(String(100), nullable=False)
+    userName = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    bio = Column(String(1000), nullable=True)
+    clave = Column(String(20), nullable=False)
+    foto = Column(String(500), nullable=True)
+    sexo = Column(String(1), nullable=False)
+    
+class seguidor(Base):
+    __tablename__ = 'seguidor'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    usuarioID = Column(Integer,  ForeignKey('usuario.id'))
+    usuarioSeguidorID = Column(Integer, ForeignKey('usuario.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
+class post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    usuarioID = Column(Integer, ForeignKey('usuario.id'))
+    fecha = Column(Datetime, nullable=False)
+    foto = Column(String(500), nullable=False)
+    caption = Column(String(500), nullable=True)
 
-    def to_dict(self):
-        return {}
+class comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    postID = Column(Integer, ForeignKey('post.id'))
+    fechaComentario = Column(Datetime, nullable=False)
+    foto = Column(String(500), nullable=False)
+    caption = Column(String(500), nullable=True)
+    usuarioID = Column(Integer, ForeignKey('usuario.id'))
+    textoComment = Column(String(500), nullable=False)
+
 
 ## Draw from SQLAlchemy base
 try:
